61ceffef21e9b42cbf6b9226f0c6c09abfd1447c
beatrizpotenza <potenzambia@gmail.com>
2025-04-19 14:55:29 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,9 @@ import { NextConfig } from 'next';
 const config: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Explicitly tell Next.js to use the src directory
   experimental: {
     appDir: true,
   },
 };
 
 export default config;
-
